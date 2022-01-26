46ca5374a1d275cc461110817e84f613c3fa07e8
Longman <Longman.Dainita@mayo.edu>
2022-01-25 19:51:01 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -27,7 +27,7 @@ with open(csvpath) as election_data:
 for candidate_name in candidate_votes:
   votes = candidate_votes[candidate_name]
   vote_percentage = float(votes)/float(total_votes) * 100
-  print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
   if (votes > winning_count) and (vote_percentage > winning_percentage):
      winning_count = votes
      winning_percentage = vote_percentage
