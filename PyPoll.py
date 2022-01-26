a002f87f763408edd0f7a7bc2c618aa758080378
Longman <Longman.Dainita@mayo.edu>
2022-01-25 20:41:37 -0500

    Adding changes

--- PyPoll.py
+++ PyPoll.py
@@ -17,33 +17,38 @@ with open(csvpath) as election_data:
   headers = next(file_reader)
   
   for row in file_reader:
-   total_votes += 1
-   candidate_name = row[2]
-   if candidate_name not in candidate_options:
-     candidate_options.append(candidate_name)
-     candidate_votes[candidate_name] = 0
-   candidate_votes[candidate_name] += 1
+    total_votes += 1
+    candidate_name = row[2]
+    if candidate_name not in candidate_options:
+      candidate_options.append(candidate_name)
+      candidate_votes[candidate_name] = 0
+    candidate_votes[candidate_name] += 1
+
+with open(election_results, "w") as text_file: 
+    election_results = (
+      f"\nElection Results\n"
+      f"-------------------------\n"
+      f"Total Votes: {total_votes:,}\n"
+      f"-------------------------\n")
+print(election_results, end="")
+text_file.write(election_results)
 
 for candidate_name in candidate_votes:
-  votes = candidate_votes[candidate_name]
-  vote_percentage = float(votes)/float(total_votes) * 100
-print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
-  if (votes > winning_count) and (vote_percentage > winning_percentage):
-     winning_count = votes
-     winning_percentage = vote_percentage
-     winning_candidate = candidate_name
-  
-if (votes > winning_count) and (vote_percentage > winning_percentage):
-    winning_count = votes
-    winning_percentage = vote_percentage        
-    winning_candidate = candidate_name
+      votes = candidate_votes[candidate_name]
+      vote_percentage = float(votes)/float(total_votes) * 100
+    #print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    
+      if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage        
+        winning_candidate = candidate_name
 
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
+      winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
 
-print(winning_candidate_summary)
+    #print(winning_candidate_summary)
 
